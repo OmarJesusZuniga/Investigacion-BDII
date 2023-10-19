d438faeb533986944035d41d670201bd5dbb6222
OmarJesusZuniga <omzuniga@estudiantec.cr>
2023-10-19 00:28:51 -0600

    90%

--- app.js
+++ app.js
@@ -10,9 +10,30 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.get('/register', function (req, res) {
+  res.render('register');
+});
+
+app.get('/home', function (req, res) {
+  res.render('home');
+});
+
+app.get('/addWave', function (req, res) {
+  res.render('wave');
+});
+
 app.get('/about', function (req, res) {
     res.render('about');
-  });
+});
+
+app.get('/friend', function (req, res) {
+  res.render('friend');
+});
+
+app.get('/account', function (req, res) {
+  res.render('account');
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
